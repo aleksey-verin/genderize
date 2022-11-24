354aa85fd29ff5bc84bb59a51e25dec1a71e8d08
Aleksey Verin <verevaa@yandex.ru>
2022-11-24 20:39:29 +0300

    перевел на русский и добавил автозамену кириллицы

--- main.js
+++ main.js
@@ -10,23 +10,70 @@ document.querySelector(".form").addEventListener("submit", function (event) {
   }
 });
 
-function showResult(input) {
+function rus_to_latin ( str ) {
+    
+  var ru = {
+      'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 
+      'е': 'e', 'ё': 'e', 'ж': 'j', 'з': 'z', 'и': 'i', 
+      'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 
+      'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u', 
+      'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'ch', 'ш': 'sh', 
+      'щ': 'shch', 'ы': 'y', 'э': 'e', 'ю': 'u', 'я': 'ya',
+  'ъ': 'ie', 'ь': '', 'й': 'i'
+  }, n_str = [];
+      
+  for ( var i = 0; i < str.length; ++i ) {
+     n_str.push(
+            ru[ str[i] ]
+         || ru[ str[i].toLowerCase() ] == undefined && str[i]
+         || ru[ str[i].toLowerCase() ].replace(/^(.)/, function ( match ) { return match.toUpperCase() })
+     );
+  }
   
-  const firstName = input.value;
+  return n_str.join('');
+}
+
+// const str = 'No Dsdsd Я Тестовая СТРОКА... И во мне есть все, ну или почти, элементы знаков припинания!';
+// console.log(rus_to_latin(str)); //Ya Testovaya STROKA... I vo mne est vse, nu ili pochti, elementy znakov pripinaniya!
+
+
+
+
+
+
+function showResult(input) {
+
+  // console.log(rus_to_latin(input.value))
+
+  let newNameForRus = rus_to_latin(input.value)
+  console.log(newNameForRus)
+
+  let nameAAAA = input.value
+  const firstName = newNameForRus;
   const serverUrl = 'https://api.genderize.io';
   const url = `${serverUrl}?name=${firstName}`;
-
+  
+  // console.log(input.value)
   let response = fetch(url)
   response
   .then(response => response.json())
   .then(function(data) {
-
-    let nameForScreen = data.name[0].toUpperCase() + data.name.slice(1)
+    let nameForScreen = nameAAAA[0].toUpperCase() + nameAAAA.slice(1)
     let probabilityForScreen = (data.probability * 100).toFixed(0)
+    console.log(nameAAAA)
     
     if (data.gender) {
-      document.querySelector('.text_result').innerHTML = `<span>${nameForScreen}</span> is <span>${data.gender}</span>`
-      document.querySelector('.probability').textContent = `Probability: ${probabilityForScreen}%`
+      // console.log(input.value)
+      let pol
+      if (data.gender === 'male') {
+        pol = 'мужчина'
+      } else {
+        pol = 'женщина'
+      }
+
+
+      document.querySelector('.text_result').innerHTML = `<span>${nameForScreen}</span> - <span>${pol}</span>`
+      document.querySelector('.probability').textContent = `С вероятностью: ${probabilityForScreen}%`
     } else {
       document.querySelector('.text_result').innerHTML = `Имя <span>${nameForScreen}</span> </br><span>не найдено</span>`
       document.querySelector('.probability').textContent = ''
@@ -37,6 +84,5 @@ function showResult(input) {
     document.querySelector('.probability').textContent = ''
   })
 }
-
 
 
