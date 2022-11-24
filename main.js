f41362a6203be351ba1ba82a9fee0e836ee0fd4d
Aleksey Verin <verevaa@yandex.ru>
2022-11-24 21:04:06 +0300

    небольшие правки в коде

--- main.js
+++ main.js
@@ -33,46 +33,32 @@ function rus_to_latin ( str ) {
   return n_str.join('');
 }
 
-// const str = 'No Dsdsd Я Тестовая СТРОКА... И во мне есть все, ну или почти, элементы знаков припинания!';
-// console.log(rus_to_latin(str)); //Ya Testovaya STROKA... I vo mne est vse, nu ili pochti, elementy znakov pripinaniya!
-
-
-
-
-
-
 function showResult(input) {
 
-  // console.log(rus_to_latin(input.value))
+  const nameWithoutTranslate = input.value
+  const nameAfterTranslate = rus_to_latin(input.value)
 
-  let newNameForRus = rus_to_latin(input.value)
-  console.log(newNameForRus)
-
-  let nameAAAA = input.value
-  const firstName = newNameForRus;
+  const firstName = nameAfterTranslate;
   const serverUrl = 'https://api.genderize.io';
   const url = `${serverUrl}?name=${firstName}`;
   
-  // console.log(input.value)
   let response = fetch(url)
   response
   .then(response => response.json())
   .then(function(data) {
-    let nameForScreen = nameAAAA[0].toUpperCase() + nameAAAA.slice(1)
+    let nameForScreen = nameWithoutTranslate[0].toUpperCase() + nameWithoutTranslate.slice(1)
     let probabilityForScreen = (data.probability * 100).toFixed(0)
-    console.log(nameAAAA)
     
     if (data.gender) {
-      // console.log(input.value)
-      let pol
+
+      let maleAfterTranstate
       if (data.gender === 'male') {
-        pol = 'мужчина'
+        maleAfterTranstate = 'мужчина'
       } else {
-        pol = 'женщина'
+        maleAfterTranstate = 'женщина'
       }
-
-
-      document.querySelector('.text_result').innerHTML = `<span>${nameForScreen}</span> - <span>${pol}</span>`
+      
+      document.querySelector('.text_result').innerHTML = `<span>${nameForScreen}</span> - <span>${maleAfterTranstate}</span>`
       document.querySelector('.probability').textContent = `С вероятностью: ${probabilityForScreen}%`
     } else {
       document.querySelector('.text_result').innerHTML = `Имя <span>${nameForScreen}</span> </br><span>не найдено</span>`
