3e70dc4e51beb36238faa27c44559edd7c6267b2
Aleksey Verin <verevaa@yandex.ru>
2022-11-25 13:27:57 +0300

    рефакторинг кода

--- main.js
+++ main.js
@@ -1,74 +1,81 @@
-document.querySelector(".form").addEventListener("submit", function (event) {
-  
-  let targetInput = event.target.firstElementChild
-  if (targetInput.value !== '') {
-    event.preventDefault();
-    showResult(targetInput)
-    targetInput.value = ''
-  } else {
-    event.preventDefault();
-  }
-});
+const ELEMENTS_FROM_UI = {
+  FORM: document.querySelector(".form"),
+  INPUT: document.querySelector(".input_name"),
+  RESULT_MAIN: document.querySelector('.text_result'),
+  RESULT_PROBABILITY: document.querySelector('.probability'),
+}
 
-function rus_to_latin ( str ) {
+ELEMENTS_FROM_UI.FORM.addEventListener("submit", getMale);
+
+function getMale(event) {
+  if (ELEMENTS_FROM_UI.INPUT.value !== '') {
+    event.preventDefault();
+    let nameInLatin = cyrillicToLatin(ELEMENTS_FROM_UI.INPUT.value)
+    getResult(nameInLatin, ELEMENTS_FROM_UI.INPUT.value)
+    ELEMENTS_FROM_UI.INPUT.value = ''
+  }
+}
+
+
+function cyrillicToLatin (anyString) {
     
-  var ru = {
+  let rusToEng = {
       'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 
       'е': 'e', 'ё': 'e', 'ж': 'j', 'з': 'z', 'и': 'i', 
       'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 
       'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u', 
       'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'ch', 'ш': 'sh', 
       'щ': 'shch', 'ы': 'y', 'э': 'e', 'ю': 'u', 'я': 'ya',
-  'ъ': 'ie', 'ь': '', 'й': 'i'
-  }, n_str = [];
+      'ъ': 'ie', 'ь': '', 'й': 'i'
+  };
+  let newString = [];
       
-  for ( var i = 0; i < str.length; ++i ) {
-     n_str.push(
-            ru[ str[i] ]
-         || ru[ str[i].toLowerCase() ] == undefined && str[i]
-         || ru[ str[i].toLowerCase() ].replace(/^(.)/, function ( match ) { return match.toUpperCase() })
+  for ( let i = 0; i < anyString.length; ++i ) {
+     newString.push(
+            rusToEng[ anyString[i] ]
+         || rusToEng[ anyString[i].toLowerCase() ] == undefined && anyString[i]
+         || rusToEng[ anyString[i].toLowerCase() ].replace(/^(.)/, function ( match ) { return match.toUpperCase() })
      );
   }
   
-  return n_str.join('');
+  return newString.join('');
 }
 
-function showResult(input) {
+function getResult(nameInLatin, nameFromInput) {
 
-  const nameWithoutTranslate = input.value
-  const nameAfterTranslate = rus_to_latin(input.value)
-
-  const firstName = nameAfterTranslate;
+  const firstName = nameInLatin;
   const serverUrl = 'https://api.genderize.io';
   const url = `${serverUrl}?name=${firstName}`;
   
   let response = fetch(url)
   response
   .then(response => response.json())
-  .then(function(data) {
-    let nameForScreen = nameWithoutTranslate[0].toUpperCase() + nameWithoutTranslate.slice(1)
-    let probabilityForScreen = (data.probability * 100).toFixed(0)
-    
-    if (data.gender) {
-
-      let genderAfterTranslate
-      if (data.gender === 'male') {
-        genderAfterTranslate = 'мужчина'
-      } else {
-        genderAfterTranslate = 'женщина'
-      }
-      
-      document.querySelector('.text_result').innerHTML = `<span>${nameForScreen}</span> - <span>${genderAfterTranslate}</span>`
-      document.querySelector('.probability').textContent = `С вероятностью: ${probabilityForScreen}%`
-    } else {
-      document.querySelector('.text_result').innerHTML = `Имя <span>${nameForScreen}</span> </br><span>не найдено</span>`
-      document.querySelector('.probability').textContent = ''
-    }
-  })
+  .then(data => showResult(nameFromInput, data.gender, data.probability))
   .catch(function(err) {
-    document.querySelector('.text_result').textContent = `Сервер не отвечает. Попробуйте позже`
-    document.querySelector('.probability').textContent = ''
+    ELEMENTS_FROM_UI.RESULT_MAIN.textContent = `Сервер не отвечает. Попробуйте позже`
+    ELEMENTS_FROM_UI.RESULT_PROBABILITY.textContent = ''
   })
+}
+
+function showResult(nameFromInput, gender, probability) {
+
+  let nameForScreen = (nameFromInput.toLowerCase())[0].toUpperCase() + (nameFromInput.toLowerCase()).slice(1)
+  let probabilityForScreen = (probability * 100).toFixed(0)
+
+  switch (gender) {
+    case 'male':
+      ELEMENTS_FROM_UI.RESULT_MAIN.innerHTML = `<span>${nameForScreen}</span> - <span>мужчина</span>`
+      ELEMENTS_FROM_UI.RESULT_PROBABILITY.textContent = `С вероятностью: ${probabilityForScreen}%`
+      break;
+    case 'female':
+      ELEMENTS_FROM_UI.RESULT_MAIN.innerHTML = `<span>${nameForScreen}</span> - <span>женщина</span>`
+      ELEMENTS_FROM_UI.RESULT_PROBABILITY.textContent = `С вероятностью: ${probabilityForScreen}%`
+      break;
+    default:
+      ELEMENTS_FROM_UI.RESULT_MAIN.innerHTML = `Имя <span>${nameForScreen}</span> </br><span>не найдено</span>`
+      ELEMENTS_FROM_UI.RESULT_PROBABILITY.textContent = ''
+      break;
+  }
 }
 
 
